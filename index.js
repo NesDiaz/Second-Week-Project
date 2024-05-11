3521f41bede2b950bf102699d88ec5342a48f1a9
Vanessa <dnessa92@yahoo.com>
2024-05-10 21:27:12 -0700

    Final Js

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
 window.onload = function () {
-  // Function to toggle hamburger icon and mobile menu
   const toggleMenu = () => {
-    const menu_btn = document.querySelector('.hamburger');
-    const mobile_menu = document.querySelector('.mobile-nav');
+    const menuBtn = document.querySelector('.hamburger');
+    const mobileMenu = document.querySelector('.mobile-nav');
 
-    // Toggle classes for menu button and mobile menu
-    menu_btn.classList.toggle('is-active');
-    mobile_menu.classList.toggle('is-active');
+    menuBtn.classList.toggle('is-active');
+    mobileMenu.classList.toggle('is-active');
 
-    // Toggle hamburger icon between burger and X
-    if (menu_btn.classList.contains('is-active')) {
-      // Change the bars to an X and add fa-xmark class
-      menu_btn.innerHTML = '<i class="fas fa-xmark"></i>';
-    } else {
-      // Change back to the burger icon and remove fa-xmark class
-      menu_btn.innerHTML = '<i class="fas fa-bars"></i>';
-    }
+    const icon = menuBtn.querySelector('i');
+    icon.classList.toggle('fa-bars');
+    icon.classList.toggle('fa-xmark');
   };
 
-  // Add click event listener to hamburger icon
   document.querySelector('.hamburger').addEventListener('click', toggleMenu);
 };
-
