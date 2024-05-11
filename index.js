90510732fd90654ce3566cd55dc6e1ba3b377d94
Vanessa <dnessa92@yahoo.com>
2024-05-10 21:17:47 -0700

    Project complete

--- index.js
+++ index.js
@@ -1,36 +1,24 @@
 window.onload = function () {
-  window.addEventListener('scroll', function (e) {
-    if (window.pageYOffset > 100) {
-      document.querySelector("header").classList.add('is-scrolling');
-    } else {
-      document.querySelector("header").classList.remove('is-scrolling');
-    }
-  });
+  // Function to toggle hamburger icon and mobile menu
+  const toggleMenu = () => {
+    const menu_btn = document.querySelector('.hamburger');
+    const mobile_menu = document.querySelector('.mobile-nav');
 
-  const menu_btn = document.querySelector('.hamburger');
-  const mobile_menu = document.querySelector('.mobile-nav');
-
-  menu_btn.addEventListener('click', function () {
+    // Toggle classes for menu button and mobile menu
     menu_btn.classList.toggle('is-active');
     mobile_menu.classList.toggle('is-active');
 
     // Toggle hamburger icon between burger and X
     if (menu_btn.classList.contains('is-active')) {
-      // Change the bars to an X
-      menu_btn.innerHTML = '<div class="bar"></div><div class="bar"></div><div class="bar"></div>';
+      // Change the bars to an X and add fa-xmark class
+      menu_btn.innerHTML = '<i class="fas fa-xmark"></i>';
     } else {
-      // Change back to the burger icon
-      menu_btn.innerHTML = '<div class="bar"></div>';
+      // Change back to the burger icon and remove fa-xmark class
+      menu_btn.innerHTML = '<i class="fas fa-bars"></i>';
     }
-  });
-}
+  };
 
-const hamburger = document.querySelector(".hamburger");
-const NavLink__Container = document.querySelector(".NavLink__Container");
-
-hamburger.addEventListener ("click", () => {
-    hamburger.classList.toggle("is-active");
-    NavLink__Container.classList.toggle("is-active");
-  })
-
+  // Add click event listener to hamburger icon
+  document.querySelector('.hamburger').addEventListener('click', toggleMenu);
+};
 
